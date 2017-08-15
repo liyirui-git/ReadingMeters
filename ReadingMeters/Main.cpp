e9b98d87d1bfe0fb45a2a9fa99ec8bdeb2d94623
iry-lee <1102951428@qq.com>
2017-08-15 10:27:19 +0800

    add ideas.txt, 整理了思路。

--- ReadingMeters/Main.cpp
+++ ReadingMeters/Main.cpp
@@ -1,10 +1,22 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <iostream>
+#include <string>
 #include <cv.h>
+#include <highgui.h>
+#include <cvaux.h>
 
+//using namespace std;
 
 int main()
 {
-	
+	IplImage* img = 0;
+	cvNamedWindow("win", CV_WINDOW_AUTOSIZE);
+	img = cvLoadImage("5.png");
+	if (!img) printf("Can't load image.\n");
+	cvShowImage("win", img);
+
+	cvWaitKey();
+
+	cvDestroyWindow("win");
+	cvReleaseImage(&img);
+
+	return 0;
 }
