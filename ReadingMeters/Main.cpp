a576bce58d3ca9a49b09f330c43aff17dc2f9620
iry-lee <1102951428@qq.com>
2017-08-21 15:26:00 +0800

    完成灰度化，均衡化，降噪以及边缘检测

--- ReadingMeters/Main.cpp
+++ ReadingMeters/Main.cpp
@@ -1,22 +1,45 @@
-#include <string>
+﻿#include <stdio.h>
 #include <cv.h>
 #include <highgui.h>
 #include <cvaux.h>
+#include <opencv2/opencv.hpp>
+#include <Windows.h>
 
-//using namespace std;
+#define WINDOW_CHANGABLESIZE 0
+#define CANNY_THRESHOLD_1 45
+#define CANNY_THRESHOLD_2 90
+#define MEDIAN_BLUR_SIZE 7
 
-int main()
-{
-	IplImage* img = 0;
-	cvNamedWindow("win", CV_WINDOW_AUTOSIZE);
-	img = cvLoadImage("5.png");
-	if (!img) printf("Can't load image.\n");
-	cvShowImage("win", img);
+using namespace std;
+using namespace cv;
 
-	cvWaitKey();
+void preprocess(void) {
 
-	cvDestroyWindow("win");
-	cvReleaseImage(&img);
+}
+
+int main() {
+	printf("\n\n<<<<<<<<<<<<  ReadingMeters v0.0.1  >>>>>>>>>>>>\n\n");
+
+	int pic_num = 1;
+
+	Mat srcImage = imread("3.jpg", 1);
+
+	//Greyed
+	Mat greyImage;
+	Mat dstImage;
+
+	cvtColor(srcImage, greyImage, CV_BGR2GRAY);
+	//Histogram-equalization
+	equalizeHist(greyImage, dstImage);
+	//Median Filter
+	medianBlur(dstImage, dstImage, MEDIAN_BLUR_SIZE);
+	//Edge Detection
+	Canny(dstImage, dstImage, CANNY_THRESHOLD_1, CANNY_THRESHOLD_2);
+
+	imwrite("3_Canny.jpg", dstImage);
+	
+	printf("-END-\n\n");
 
 	return 0;
 }
+
